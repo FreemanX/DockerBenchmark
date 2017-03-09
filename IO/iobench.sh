03261ab9ff67e54ed6be1bee0314270f4da6fcb6
FreemanX <gzgyxpf@gmail.com>
2017-03-09 09:55:01 +0000

    IO test change to -R

--- IO/iobench.sh
+++ IO/iobench.sh
@@ -8,13 +8,17 @@ echo "[2] ioping -c 10 IO latency test"
 ioping -c 10 .
 
 echo "[3] ioping cached IO random access test (5 sec)"
-ioping -C -q -i 0 -S 256m -w 5 .
+#ioping -C -q -i 0 -S 256m -w 5 .
+ioping -C -R -w 5 .
 
 echo "[4] ioping cached IO sequential access test (5 sec)"
-ioping -C -q -i 0 -S 256m -w 5 -s 1m .
+#ioping -C -q -i 0 -S 256m -w 5 -s 1m .
+ioping -C -RL -w 5 .
 
 echo "[5] ioping direct IO random access test (5 sec)"
-ioping -D -q -i 0 -S 256m -w 5 .
+#ioping -D -q -i 0 -S 256m -w 5 .
+ioping -D -R -w 5 .
 
 echo "[6] ioping direct IO sequential access test (5 sec)"
-ioping -D -q -i 0 -S 256m -w 5 -s 1m .
+#ioping -D -q -i 0 -S 256m -w 5 -s 1m .
+ioping -D -RL -w 5 .
