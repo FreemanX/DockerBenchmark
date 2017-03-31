ac9308f6dc485f58458679ad2f604ea6ee5bccbe
FreemanX <gzgyxpf@gmail.com>
2017-04-01 01:00:50 +0800

    log parser for ddbench added

---
+++ IO/ddbench_parser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$filename" ]
+then
+	echo "You need to pass the IO log file name like:"
+	echo "filename=<your file name> ./parse_result.sh"
+	exit 1
+fi
+
+echo "======Input file: $filename====="
+for ((i=1; i<5; i++))
+do
+	cat gpuhost_ddIO.log | grep "\[$i\]" | uniq
+	cat gpuhost_ddIO.log | grep "\[$i\]" -A3 | grep bytes | cut -d',' -f3 | cut -d' ' -f2
+done
